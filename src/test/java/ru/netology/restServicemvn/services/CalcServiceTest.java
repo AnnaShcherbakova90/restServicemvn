1544e451a93d09de8437aaa706da48882ddf6bfa
Anna Shcherbakova <ShcherbakovaAnna90@yandex.ru>
2023-11-18 14:59:28 +0600

    add csv file

--- src/test/java/ru/netology/restServicemvn/services/CalcServiceTest.java
+++ src/test/java/ru/netology/restServicemvn/services/CalcServiceTest.java
@@ -2,23 +2,30 @@ package ru.netology.restServicemvn.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class CalcServiceTest {
 
-    @Test
-    public void testOne() {
+    @ParameterizedTest
+  /*  @CsvSource({
+            "3,10000,20000,3000",
+            "2,100000,150000,60000"
+    })*/
+    @CsvFileSource(files="src/test/resources/rest.csv")
+    public void testOne(int expected, int income, int threshold, int expenses) {
         CalcService restService = new CalcService();
 
-        int income = 10_000;
-        int expenses = 3000;
-        int threshold = 20_000;
-        int expected = 3;
+        //int income = 10_000;
+        //int expenses = 3000;
+        //int threshold = 20_000;
+        //int expected = 3;
         int actual = restService.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
-
-    @Test
+   /* @Test
     public void testTwo() {
         CalcService restService = new CalcService();
 
@@ -29,5 +36,5 @@ public class CalcServiceTest {
         int actual = restService.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
-    }
+    }*/
 }
